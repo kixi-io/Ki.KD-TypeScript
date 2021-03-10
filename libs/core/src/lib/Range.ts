829508ac9e1b98e540bfd8fc5d1ac61f9c51ce99
Jose Gazzano <jose@setaworkshop.com>
2021-03-10 18:11:26 -0300

    adding reverse flag for range

--- libs/core/src/lib/Range.ts
+++ libs/core/src/lib/Range.ts
@@ -1,27 +1,38 @@
 import { ParseError } from './ParseError';
 
+const underscore = '_';
+const closed = '<';
+
 export class Range {
 	min: number | string;
 	max: number | string;
 	isOpenLeft: boolean;
 	isOpenRight: boolean;
+	isReversed: boolean;
 
-	constructor(left: number | string = '_', right: number | string = '_', openLeft: boolean = true, openRight: boolean = true) {
+	constructor(
+		left: number | string = underscore,
+		right: number | string = underscore,
+		openLeft: boolean = true,
+		openRight: boolean = true
+	) {
 		this.checkValues(left, right);
-		const leftValue = left === '_' ? -Infinity : +left;
-		const rightValue = right === '_' ? Infinity : +right;
+		const leftValue = left === underscore ? -Infinity : +left;
+		const rightValue = right === underscore ? Infinity : +right;
 
-    if (leftValue > rightValue) {
-      this.min = rightValue;
-      this.max = leftValue;
-      this.isOpenLeft = openRight;
-      this.isOpenRight = openLeft;
-    } else {
-      this.min = leftValue;
-      this.max = rightValue;
-      this.isOpenLeft = openLeft;
-      this.isOpenRight = openRight;
-    }
+		this.isReversed = leftValue > rightValue;
+
+		if (leftValue > rightValue) {
+			this.min = rightValue;
+			this.max = leftValue;
+			this.isOpenLeft = openRight;
+			this.isOpenRight = openLeft;
+		} else {
+			this.min = leftValue;
+			this.max = rightValue;
+			this.isOpenLeft = openLeft;
+			this.isOpenRight = openRight;
+		}
 	}
 
 	public contains(element: number) {
@@ -39,24 +50,33 @@ export class Range {
 		const leftOperator = this.parseOperator(leftOperatorString, false);
 		const rightOperator = this.parseOperator(rightOperatorString, true);
 
-    return new Range(leftOperator.value, rightOperator.value, leftOperator.isOpen, rightOperator.isOpen);
+		return new Range(leftOperator.value, rightOperator.value, leftOperator.isOpen, rightOperator.isOpen);
 	}
 
-	equals(obj: Range) : boolean {
-	  return obj !== null && obj.min === this.min && obj.max === this.max && obj.isOpenRight === this.isOpenRight && obj.isOpenLeft === this.isOpenLeft;
+	equals(obj: Range): boolean {
+		return (
+			obj !== null &&
+			obj.min === this.min &&
+			obj.max === this.max &&
+			obj.isOpenRight === this.isOpenRight &&
+			obj.isOpenLeft === this.isOpenLeft
+		);
 	}
 
-	toString = () => `${this.min === -Infinity ? '_' : this.min}${this.isOpenLeft ? '' : '<'}..${this.isOpenRight ? '' : '<'}${this.max === Infinity ? '_' : this.max}`;
+	toString = () =>
+		`${this.min === -Infinity ? underscore : this.min}${this.isOpenLeft ? '' : closed}..${this.isOpenRight ? '' : closed}${
+			this.max === Infinity ? underscore : this.max
+		}`;
 
 	private static parseOperator(operator: string, right: boolean) {
-		const res = operator.split('<');
-    const [value, openChar] = right ? res.reverse() : res;
+		const res = operator.split(closed);
+		const [value, openChar] = right ? res.reverse() : res;
 
 		return { value, isOpen: !openChar && typeof openChar !== 'string' };
 	}
 
 	private checkValues(left: number | string, right: number | string) {
-		if (left === '_' && right === '_') {
+		if (left === underscore && right === underscore) {
 			throw new ParseError('Range requires at least one value.');
 		}
 	}
